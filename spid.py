fd9f0289a5792b8ae5ba86285fb9bf99ef2dc369
alishbajt <alishba.jafrie@gmail.com>
2019-02-06 15:09:19 +0530

    scraped 1 page of a site

---
+++ spid.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class JokesSpider(scrapy.Spider):
+    name= 'jokes'
+    start_urls= ['http://www.laughfactory.com/jokes/clean-jokes']
+
+    def parse(self, response):
+        for joke in response.xpath("//div[@class='jokes']"):
+	 yield {
+                        'joke_text': joke.xpath(".//div[@class='joke-text']/p").extract_first()
+                      }
+	
